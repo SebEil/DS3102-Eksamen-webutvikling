205b76d048acedf611d85bea2dc643287c994626
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2021-10-28 13:28:45 +0200

    Header fra arbeidskravet hentet inn. + quiz bilde fra google.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Dummy tekst her //
+const hamburgerMenu = document.querySelector(".hamburger");
+const mainNavigationContainer = document.querySelector(".navigation-container");
+
+const changeDisplay = () => {
+    
+    const display = mainNavigationContainer.style.display;
+
+    if( display === "none" || display === "" ){
+        mainNavigationContainer.style.display = "block";
+    }else{
+        mainNavigationContainer.style.display = "none";
+    }
+
+}
+
+
+hamburgerMenu.addEventListener("click", changeDisplay);
