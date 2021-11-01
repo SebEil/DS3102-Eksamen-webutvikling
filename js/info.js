62372c65261413f8d6dd8f2c5fbb7985409dbc21
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2021-11-01 16:58:00 +0100

    Spillerbilder

--- js/info.js
+++ js/info.js
@@ -1,36 +1,36 @@
 var unitedPlayers = [
     {
-        name: "David De Gea",  age: "30", country: "Spania", position: "Målvakt", image: "/Img/quizlogo.png"
+        name: "David De Gea",  age: "30", country: "Spania", position: "Målvakt", image: "/Img/ManchesterUnitedSpillere/deGea.png"
     },
     {
-        name: "Raphael Varane",  age: "28", country: "Frankrike", position: "Forsvarsspiller"
+        name: "Raphael Varane",  age: "28", country: "Frankrike", position: "Forsvarsspiller", image: "/Img/ManchesterUnitedSpillere/varane.png"
     },
     {
-        name: "Harry Maguire",  age: "28", country: "England", position: "Forsvarsspiller"
+        name: "Harry Maguire",  age: "28", country: "England", position: "Forsvarsspiller", image: "/Img/ManchesterUnitedSpillere/maguire.png"
     },
     {
-        name: "Aaron Wan-Bissaka",  age: "23", country: "England", position: "Forsvarsspiller"
+        name: "Aaron Wan-Bissaka",  age: "23", country: "England", position: "Forsvarsspiller", image: "/Img/ManchesterUnitedSpillere/wan-bissaka.png"
     },
     {
-        name: "Luke Shaw",  age: "26", country: "England", position: "Forsvarsspiller"
+        name: "Luke Shaw",  age: "26", country: "England", position: "Forsvarsspiller", image: "/Img/ManchesterUnitedSpillere/shaw.png"
     },
     {
-        name: "Frederico Rodrigues Santos",  age: "28", country: "Brazil", position: "Midtbanespiller"
+        name: "Frederico Rodrigues Santos",  age: "28", country: "Brazil", position: "Midtbanespiller", image: "/Img/ManchesterUnitedSpillere/fred.png"
     },
     {
-        name: "Paul Pogba",  age: "28", country: "France", position: "Midtbanespiller"
+        name: "Paul Pogba",  age: "28", country: "France", position: "Midtbanespiller", image: "/Img/ManchesterUnitedSpillere/pogba.png"
     },
     {
-        name: "Marcus Rashford",  age: "24", country: "England", position: "Midtbanespiller"
+        name: "Marcus Rashford",  age: "24", country: "England", position: "Midtbanespiller", image: "/Img/ManchesterUnitedSpillere/rashford.png"
     },
     {
-        name: "Bruno Miguel Borges Fernandes",  age: "27", country: "Portugal", position: "Midtbanespiller"
+        name: "Bruno Miguel Borges Fernandes",  age: "27", country: "Portugal", position: "Midtbanespiller", image: "/Img/ManchesterUnitedSpillere/bruno.png"
     },
     {
-        name: "Cristiano Ronaldo dos Santos Aveiro",  age: "36", country: "Portugal", position: "Angrepsspiller"
+        name: "Cristiano Ronaldo dos Santos Aveiro",  age: "36", country: "Portugal", position: "Angrepsspiller", image: "/Img/ManchesterUnitedSpillere/ronaldo.png"
     },
     {
-        name: "Edinson Cavani",  age: "34", country: "Uruguay", position: "Angrepsspiller"
+        name: "Edinson Cavani",  age: "34", country: "Uruguay", position: "Angrepsspiller", image: "/Img/ManchesterUnitedSpillere/cavani.png"
     },
 ]
 
