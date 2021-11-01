1317cfed47c888fbcad28ae2312e3fb042eecb2a
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2021-11-01 14:06:38 +0100

    Array fotballspillere

---
+++ js/info.js
@@ -0,0 +1,32 @@
+var unitedPlayers = [
+    {
+        name: "David De Gea",  age: "30", from: "Spain", position: "Keeper"
+    },
+    {
+        name: "Raphael Varane",  age: "28", from: "France", position: "Defender"
+    },
+    {
+        name: "Harry Maguire",  age: "28", from: "England", position: "Defenderk"
+    },
+    {
+        name: "Aaron Wan-Bissaka",  age: "23", from: "England", position: "Defender"
+    },
+    {
+        name: "Luke Shaw",  age: "26", from: "England", position: "Defender"
+    },
+    {
+        name: "Frederico Rodrigues Santos",  age: "28", from: "Brazil", position: "Midfielder"
+    },
+    {
+        name: "Paul Pogba",  age: "28", from: "France", position: "Midfielder"
+    },
+    {
+        name: "Marcus Rashford",  age: "24", from: "England", position: "Midfielder"
+    },
+    {
+        name: "Bruno Miguel Borges Fernandes",  age: "", from: "", position: ""
+    },
+    {
+        name: "",  age: "", from: "", position: ""
+    },
+]
