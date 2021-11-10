f99de062b4a608e67ef806405a73e6901fccd553
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2021-11-10 17:36:49 +0100

    endringer i quiz, lagt til liverpool, mangler å fikse output for liverpool

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 /* Variabler */
 const quizContainerUnited = document.getElementById('quiz-united');
 const quizContainerLiverpool = document.getElementById('quiz-liverpool');
-const resultsContainer = document.getElementById('results');
+const resultsUnited = document.getElementById('resultsUnited');
 const submitButton = document.getElementById('submit');
 
 /* Array for Manchester United */
@@ -77,14 +77,13 @@ const unitedQuizBox = document.querySelector("#unitedArticle")
 const liverpoolQuizBtn = document.querySelector("#liverpoolBtn")
 const liverpoolQuizBox = document.querySelector("#liverpoolArticle")
 
-
+/* Vise en av quizene og skjule den andre */
 let showUnitedQuiz = () => {
   
   unitedQuizBox.style.display = "block"
   liverpoolQuizBox.style.display = "none"
 
 }
-
 unitedQuizBtn.addEventListener("click", showUnitedQuiz)
 
 let showLiverpoolQuiz = () => {
@@ -93,91 +92,99 @@ let showLiverpoolQuiz = () => {
   liverpoolQuizBox.style.display = "block"
 
 }
-
 liverpoolQuizBtn.addEventListener("click", showLiverpoolQuiz)
+//slutt
 
 
-
-    // Functions
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
+// Funksjoner
+function buildQuizUnited(){
+  // variabel for å store output
+    const output = [];
   
-      // for each question... UNITED
-      myQuestionsUnited.forEach(
-        (currentQuestion, questionNumber) => {
+    // forEach United spørsmål
+    myQuestionsUnited.forEach(
+      (currentQuestion, questionNumber) => {
   
-          // variable to store the list of possible answers
-          const answers = [];
+        // variabel for å store mulige svar
+        const answers = [];
   
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
   
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="checkbox" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-
-          
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
+          //for hvert svar-alternativ, legg til label
+          answers.push(
+            `<label>
+              <input type="checkbox" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
           );
         }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainerUnited.innerHTML = output.join('');
 
-      // for each question...LIVERPOOL
-      myQuestionsLiverpool.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="checkbox" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
           
   
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
-          );
-        });
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
   
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainerLiverpool.innerHTML = output.join('');
-    }
+// finally combine our output list into one string of HTML and put it on the page
+quizContainerUnited.innerHTML = output.join('');
+}
 
+function buildQuizLiverpool(){
+
+
+  /* FIKS OUTPUT HER
+  
+
+  *******************************************************************************************************************************************************************************************
+  
+  */
+  const outputLiverpool = [];
+myQuestionsLiverpool.forEach(
+  (currentQuestion, questionNumber) => {
+
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for(letter in currentQuestion.answers){
+
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
+          <input type="checkbox" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+        </label>`
+      );
+    }
+      
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="slide">
+        <div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>
+      </div>`
+    );
+  });
+
+// finally combine our output list into one string of HTML and put it on the page
+quizContainerLiverpool.innerHTML = output.join('');
+}
          
   
-function showResults(){
+function showResultsUnited(){
   
       // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
+      const answerContainers = quizContainerUnited.querySelectorAll('.answers');
   
       // keep track of user's answers
       let numCorrect = 0;
@@ -204,13 +211,26 @@ function showResults(){
           answerContainers[questionNumber].style.color = 'red';
         }
       });
-      // for each question... LIVERPOOL
-      myQuestionsLiverpool.forEach( (currentQuestion, questionNumber) => {
+
+      // show number of correct answers out of total
+      resultsUnited.innerHTML = `Du fikk ${numCorrect} av ${myQuestionsUnited.length} riktige`;
+}
+
+function showResultsLiverpool(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainerLiverpool.querySelectorAll('.answers');
   
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question... LIVERPOOL
+    myQuestionsLiverpool.forEach( (currentQuestion, questionNumber) => {
+  
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
   
         // if answer is correct
         if(userAnswer === currentQuestion.correctAnswer){
@@ -228,13 +248,13 @@ function showResults(){
       });
   
       // show number of correct answers out of total
-      resultsContainer.innerHTML = `Du fikk ${numCorrect} av ${myQuestions.length} riktige`;
+      resultsLiverpool.innerHTML = `Du fikk ${numCorrect} av ${myQuestions.length} riktige`;
     }
   
-    function showSlide(n) {
-      slides[currentSlide].classList.remove('active-slide');
-      slides[n].classList.add('active-slide');
-      currentSlide = n;
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
       if(currentSlide === 0){
         previousButton.style.display = 'none';
       }
@@ -250,29 +270,32 @@ function showResults(){
         submitButton.style.display = 'none';
       }
     }
+
+    
+// Funksjoner for å vise neste slide og/eller forrige    
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
   
-    function showNextSlide() {
-      showSlide(currentSlide + 1);
-    }
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+// Kjør program
+buildQuizUnited();
+
+buildQuizLiverpool();
   
-    function showPreviousSlide() {
-      showSlide(currentSlide - 1);
-    }
+// Navigering i Quiz, HENTE INN KNAPPER FRA HTML
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
   
-  
-    // Kjør program
-    buildQuiz();
-  
-    // Navigering i Quiz
-    const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-  
-    //Vis første spørsmålsslide
-    showSlide(currentSlide);
-  
-    /* Event listeners */
-    submitButton.addEventListener('click', showResults);
-    previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
+//Vis første spørsmålsslide
+showSlide(currentSlide);
+
+/* Event listeners på knappene */
+submitButton.addEventListener('click', showResultsUnited, showResultsLiverpool);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
